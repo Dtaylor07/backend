ec6438416d09bfef4f66c2a68b2346a3d833f198
Dhaval <dhavaltailor777@gmail.com>
2025-04-13 18:37:18 -0700

    done1

--- api/index.js
+++ api/index.js
@@ -1,19 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import fetch from 'node-fetch'; // `npm install node-fetch`
+import fetch from 'node-fetch';
 
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.get('/login', (req, res) => {
   const clientId = process.env.CLIENT_ID;
   const redirectUri = process.env.REDIRECT_URI;
-
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=read:packages,write:packages`;
-
   res.redirect(githubAuthUrl);
 });
 
@@ -48,5 +44,6 @@ app.get('/github/callback', async (req, res) => {
   }
 });
 
-// ✅ THIS is how to make Express work with Vercel serverless:
+// ❌ DON'T USE app.listen()
+// ✅ Instead, export the express app for Vercel:
 export default app;
